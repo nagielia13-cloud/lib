6aff5e51d472a2d2ecd24725d41dec0d0eefdea5
Elia Nagi <nagielia13@gmail.com>
2026-01-10 10:46:03 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,7 +1,6 @@
 void main() {
   print("Hello, World!");
-  print("object")
-
-print ("This is a Dart program.");
-print("elia nagi")
-} 
+  print("object");
+  print("This is a Dart program.");
+  print("elia nagi");
+}
