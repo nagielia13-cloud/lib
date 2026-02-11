01eebee08330d2489f740dc463641d0c59291bed
Elia Nagi <nagielia13@gmail.com>
2026-02-11 05:19:37 +0200

    Create 2.dart

---
+++ 2.dart
@@ -0,0 +1,30 @@
+/*void main() {
+  int num1 = 20;
+  double num2 = 10.5;
+  double result1 = num1 + num2;
+  double result2 = num1 * num2;
+  double result3 = num1 - num2;
+  double result4 = num1 / num2;
+  double result5 = num1 % num2;
+  bool isGreater = num1 > num2;
+  bool isLess = num1 < num2;
+  print(' these is result : $num1 + $num2 = $result1');
+  print(' these is result : $num1 * $num2 = $result2');
+  print(' these is result : $num1 - $num2 = $result3');
+  print(' these is result : $num1 / $num2 = $result4');
+  print(' these is result : $num1 % $num2 = $result5');
+  print(' is $num1 greater than $num2? : $isGreater');
+  print(' is $num1 less than $num2? : $isLess');
+}*/
+/*void main() {
+  const double pi = 3.14;
+  const double radius = 5.0;
+  double area = pi * radius * radius;
+  print('The area of the circle with radius $radius is $area');
+}*/
+/*void main() {
+  String name = 'elia';
+  int age = 23;
+  String massege = 'my name is $name and my age is $age yers old';
+  print(massege);
+}*/
